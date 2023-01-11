acab367b01173fed1c31d874406e6e23ed045316
Askhad Arzybaev <aarzybaev@gmail.com>
2023-01-11 23:32:41 +0600

    feat: task is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function toReadable (number) {
-  
+    
+    if (number == 0) return 'zero';
+
+    let words = [];
+    let numberAsString = String(number);
+    let length = numberAsString.length;
+
+    let rest = length%3;
+
+    if (rest) { numberAsString =  (rest == 1) ? '00' + numberAsString : '0' + numberAsString; }
+
+    let result = numberAsString.match(/.{1,3}/g) || [];
+
+    for(let i = 0; i < result.length; i++) {
+        words.push(foo(result[i], result.length, i));
+    }
+    
+    return words.join(' ').slice(0, -1);
+
+function foo(number, length, i){
+
+    if(!(+number)) {return '';}
+
+    let selecter = length - i - 1;
+
+    let bar = ['hundred','thousand', 'million', 'billion','trillion'];
+
+    let isExpOne = false;
+    let oneToNine =     ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tens =          ['', '', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    let tenToNineteen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    
+    if (+number[0]) {
+        words.push(oneToNine[+number[0]]);
+        words.push(bar[0]);
+    }
+
+    if (+number[1] == 1) {
+        isExpOne = true;
+    } else {
+        if (+number[1]) {
+            words.push(tens[+number[1]]);
+        }
+    }
+
+    if (isExpOne) {
+        words.push(tenToNineteen[+number[2]]);
+    } else {
+            if(+number[2]) {
+                 words.push(oneToNine[+number[2]]);
+            }
+        
+    }
+
+    if (length-i > 1) {
+         words.push(bar[selecter]);
+    }
+
+}
 }
