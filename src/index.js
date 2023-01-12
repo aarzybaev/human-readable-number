27bd3f3009090ddc89f22d33134cbf2e82768ebe
aarzybaev <46291608+aarzybaev@users.noreply.github.com>
2023-01-12 11:13:04 +0600

    feat: fix some bugs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ module.exports = function toReadable (number) {
     let words = [];
     let numberAsString = String(number);
     let length = numberAsString.length;
-
     let rest = length%3;
 
     if (rest) { numberAsString =  (rest == 1) ? '00' + numberAsString : '0' + numberAsString; }
@@ -13,49 +12,47 @@ module.exports = function toReadable (number) {
     let result = numberAsString.match(/.{1,3}/g) || [];
 
     for(let i = 0; i < result.length; i++) {
-        words.push(foo(result[i], result.length, i));
+        if (foo(result[i], result.length, i))
+            words.push(foo(result[i], result.length, i));
     }
     
-    return words.join(' ').slice(0, -1);
+    return words.join(' ');
 
-function foo(number, length, i){
+    function foo(number, length, i){
 
-    if(!(+number)) {return '';}
+        if(!(+number)) { return ''; }
 
-    let selecter = length - i - 1;
+        let selecter = length - i - 1;
+        let isExpOne = false;
+        let bar =           ['hundred','thousand', 'million', 'billion','trillion'];
+        let oneToNine =     ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        let tens =          ['', '', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+        let tenToNineteen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
 
-    let bar = ['hundred','thousand', 'million', 'billion','trillion'];
-
-    let isExpOne = false;
-    let oneToNine =     ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let tens =          ['', '', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-    let tenToNineteen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    
-    if (+number[0]) {
-        words.push(oneToNine[+number[0]]);
-        words.push(bar[0]);
-    }
-
-    if (+number[1] == 1) {
-        isExpOne = true;
-    } else {
-        if (+number[1]) {
-            words.push(tens[+number[1]]);
+        if (+number[0]) {
+            words.push(oneToNine[+number[0]]);
+            words.push(bar[0]);
         }
-    }
 
-    if (isExpOne) {
-        words.push(tenToNineteen[+number[2]]);
-    } else {
-            if(+number[2]) {
-                 words.push(oneToNine[+number[2]]);
+        if (+number[1] == 1) {
+            isExpOne = true;
+        } else {
+            if (+number[1]) {
+                words.push(tens[+number[1]]);
             }
-        
-    }
+        }
 
-    if (length-i > 1) {
-         words.push(bar[selecter]);
-    }
+        if (isExpOne) {
+            words.push(tenToNineteen[+number[2]]);
+        } else {
+                if(+number[2]) {
+                     words.push(oneToNine[+number[2]]);
+                }        
+        }
 
-}
+        if (length-i > 1) {
+             words.push(bar[selecter]);
+        }
+
+    }
 }
